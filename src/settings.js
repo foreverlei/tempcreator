374e75e1a0cd00cd6f0cec2749bf95682f170e03
Lei <516292026@qq.com>
2019-03-14 10:30:36 +0800

    提交备份

---
+++ src/settings.js
@@ -0,0 +1,120 @@
+window._CCSettings = {
+    platform: "android",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {}
+    },
+    launchScene: "db://assets/scenes/menu.fire",
+    scenes: [
+        {
+            url: "db://assets/scenes/menu.fire",
+            uuid: "8aVGk1cNRC0qBRjQ528oAI"
+        }
+    ],
+    packedAssets: {
+        "0379fb962": [
+            "0e2M9ujARFaY0XYmom4Qmf",
+            "13fRym6QxEC5+iS5//9Wn3",
+            "151fPw+WVMAJRb0sj67ni2",
+            "19UJux3AhMv6uPJGDiByZc",
+            "266ahnPS9JgYozgtRA3nr/",
+            "2aQOXnTEpDUJ5ddnV3Vc3S",
+            "2dhqhUY8RLkIuIpDKLhSbC",
+            "2d3+AFISlB2K7sKx9R8Cli",
+            "34Was2eCxMTorvcoCv+LJy",
+            "36tupz/0hDDqDHDl6N7+NB",
+            "39S6yCVPtHL6J/tRB4Ib+4",
+            "3c7bi0hTJAN6AOuNPgATFY",
+            "4eBsfxcqxOTpDeaD4WkFFW",
+            "502lSG36FG0p1PaG61Unwa",
+            "52ReJcAQxF+4Sj87zpV5Pn",
+            "6d4Goj0N5HZqnhoDFBNtYu",
+            "70D6oXEaZGza611pALwmT4",
+            "71VhFCTINJM6/Ky3oX9nBT",
+            "80cd+dAptA6JjzjqsI2/bK",
+            "82/ljUrhNIBppBLnOQLqgR",
+            "83zCCG1xNHoI2GqNYGi2JY",
+            "964+KTTjZCbaCm640CXA1b",
+            "97pjFsf8tP/pBFNWJbxqv2",
+            "97u5ycVWhEGa8ETtWilpoC",
+            "99FwsL0hBG8bITfZ4/IwmK",
+            "99rMcWM99MTIedzDQH8M2M",
+            "acEUOdN1hJ9Ycoge0iwe2W",
+            "aeTiGIK3tCqYXhj7mHYAsE",
+            "af4ymm6F5GoJjtijThKJB7",
+            "b2A380BP9DUbnaW+S7VXAX",
+            "b4P/PCArtIdIH38t6mlw8Y",
+            "c3nqSWlutNxZRa58kZt3wh",
+            "caqvn/UDZCMqiniLgLLkyI",
+            "cayvqF2OlHFrzbfrpFfkCc",
+            "cebS3pcFZLqKGxQLALtvRp",
+            "cf73jxyN9Jt47QTJU6ziYh",
+            "d53+ao6xlKrqdPg7ceqlfc",
+            "daPlVvG85MMYfciX6i14ji",
+            "e8Ueib+qJEhL6mXAHdnwbi",
+            "ecJE7lbx9JIJtp1N8OeOwt",
+            "fczE2FatRJbZsz6nbmnaEy"
+        ],
+        "0cd5de143": [
+            "02KoCrTN5Cyp4EiiN0XPE4",
+            "11yLJ+KaBML6wxA6LleTt8",
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "2emy3AZbRE5bYk1h2p7hCV",
+            "31051rd4RCarG7NwSS8NX2",
+            "413CCm1gpPVYVOhg0q1jSL",
+            "450PiT97dMhIvZ3WQJ7G3p",
+            "57F8d8Zd5CMpcUsMZj33ON",
+            "5aftkBvcFII4sN0tuNRQbq",
+            "60NfrGUghOC76nYv+fsTOL",
+            "67nkSrGW9KU5z6ypunyLpD",
+            "675ovJ2tVK2aLYfgPUWOMv",
+            "7a7hhmAdtBqq/p5KeR3vaq",
+            "7dHU5gq6JI6IX4jjKPNOfM",
+            "7d4k6EIb1DS4yisDHcu0OO",
+            "8855/VlrRKd6H0MSRnFxAU",
+            "8aVGk1cNRC0qBRjQ528oAI",
+            "93V1DuKiVNYKEPBrMADiir",
+            "a22nm+/iJCCoO143h4NnsG",
+            "a8sHzPEydJMbIa0yfNDizc",
+            "c0FGbqcoNPzrYVTueMd0rw",
+            "c20POSKPtHp6r5r0DnX0DT",
+            "cafdc9UmpMhZcC61HpO52Z",
+            "d1kzAXyEVJLapyv3HXtIcm",
+            "d5iFnbamRIm6ocaD5fmmRr",
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "efDpLYbbNOAaHwWYi/crTs",
+            "f0BIwQ8D5Ml7nTNQbh1YlS"
+        ],
+        "0d756af45": [
+            "0bx/EMg6BAH7FGUY54r2WT",
+            "d4dRnPIMREsLxXEcluUu/d"
+        ],
+        "0dc6a4e59": [
+            "25sqYz8mlC0oD4EQ6DQjjr",
+            "268eG5oMZBpqj/MQLzy8eE",
+            "3ffW8Ujj9DLKU3CcjH4Dt2",
+            "42uEUGVbRLmI0us/jP5/v4",
+            "554KlKsjZMa44NMi4ZjiSj",
+            "5b/It2XrZM+ag6JKUIEDUS",
+            "65+N1EnAxIG5t4whkwegUl",
+            "74WfyONyNNlYWUrI7qmfJ+",
+            "8a941lS0FNWaaDJlmraQTL",
+            "91PhIbd3BFk5o0+MobPjsv",
+            "94SxcmxnlIxph8KrtJ+aEe",
+            "a5hWr9MP9D5ZP2Qei+LQnR",
+            "e48h5V7U9G+Lmhful3XXEM",
+            "efKHgzRZ9P9KQ6em3cjJWp"
+        ]
+    },
+    md5AssetsMap: {},
+    orientation: "",
+    debug: true,
+    subpackages: {}
+};
